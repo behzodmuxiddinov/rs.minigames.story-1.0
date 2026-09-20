f159fdc9fd8620bb481595e08ef862817f4f9687
behzodmuxiddinov <behzodmufc@gmail.com>
2026-09-20 18:52:55 +0500

    chore: formatCompact function

--- src/utils/formatNumber.utils.ts
+++ src/utils/formatNumber.utils.ts
@@ -2,3 +2,8 @@ export const formatNumber = (number: number): string => {
   if (!number) return '';
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
+
+export const formatCompact = (number: number): string => {
+  if (number < 1000) return number.toString();
+  return `${Math.floor(number / 100) / 10}K`;
+};
